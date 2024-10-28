6671efd697841794107409ef2129f891c6078c91
myhauntedspoon <drewhinckley2009@gmail.com>
2024-10-28 09:05:31 -0500

    add more inputs

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert(
+        "Form submitted successfully. Thank you for joining Scammer Support!"
+    );
+});
